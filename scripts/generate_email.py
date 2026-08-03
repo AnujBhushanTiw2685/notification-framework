ce498005b6b11c695842155f9a74b9be7e514926
AnujBhushanTiw2685 <anujbhushantiwari2022@vitbhopal.ac.in>
2026-08-03 05:07:14 +0000

    update email_template.html and remove python html placeholder from generate_email.py

--- scripts/generate_email.py
+++ scripts/generate_email.py
@@ -9,40 +9,16 @@ with open("artifacts/failure_summary.json", "r") as file:
     error = failure["error"]
     exit_code = failure["exit_code"]
 
-html = f"""
-<!DOCTYPE html>
-<html>
+with open("templates/email_template.html", "r", encoding="utf-8") as file:
+    html = file.read()
 
-<head>
-    <Title> CI/CD Failure Notification </Title>
-</head>
-
-<body>
-
-<h1>CI/CD Failure Notification</h1>
-
-<hr>
-
-<p><b>Component:</b>{component}</p>
-<p><b>Job:</b>{job}</p>
-<p><b>Status:</b>{status}</p>
-<p><b>Time:</b>{timestamp}</p>
-
-<hr>
-
-<h3>Error</h3>
-
-<pre>{error}</pre>
-
-<hr>
-
-<p><b>Exit Code:</b>{exit_code}</p>
-
-</body>
-
-</html>
-
-"""
+# Replace placeholders with actual values
+html = html.replace("{{COMPONENT}}", component)
+html = html.replace("{{JOB}}", job)
+html = html.replace("{{STATUS}}", status)
+html = html.replace("{{TIMESTAMP}}", timestamp)
+html = html.replace("{{ERROR}}", error)
+html = html.replace("{{EXIT_CODE}}", exit_code)
 
 with open("artifacts/notification.html", "w", encoding="utf-8") as file:
     file.write(html)
