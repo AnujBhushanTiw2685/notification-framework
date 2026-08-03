013ee76fff5831e04119daf005aa15d688154188
AnujBhushanTiw2685 <anujbhushantiwari2022@vitbhopal.ac.in>
2026-08-03 08:54:06 +0000

    feat: modify notify.yml with Email sender job, update smtp_config.json with outlook config , modify smtp sender script file

---
+++ scripts/smtp_sender.py
@@ -0,0 +1,65 @@
+import os
+import json
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+with open("config/smtp_config.json", "r") as file:
+    config = json.load(file)
+
+SMTP_SERVER = config["smtp_server"]
+SMTP_PORT = config["smtp_port"]
+SENDER_NAME = config["sender_name"]
+RECIPIENTS = config["recipients"]
+
+SMTP_EMAIL = os.environ["SMTP_EMAIL"]
+SMTP_PASSWORD = os.environ["SMTP_PASSWORD"]
+
+with open("artifacts/notification.html", "r", encoding="utf-8") as file:
+    html_body = file.read()
+
+with open("artifacts/failure_summary.json", "r") as file:
+    summary = json.load(file)
+
+repository = summary["repository"]
+job = summary["job"]
+workflow = summary["workflow"]
+
+subject = (
+    f"🚨 CI/CD Failure | "
+    f"{repository} | "
+    f"{job}"
+)
+
+message = MIMEMultipart("alternative")
+message["Subject"] = subject
+message["From"] = f"{SENDER_NAME} <{SMTP_EMAIL}>"
+message["To"] = ", ".join(RECIPIENTS)
+
+message.attach(MIMEText(html_body, "html"))
+
+try:
+    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    server.starttls()
+
+    server.login(SMTP_EMAIL, SMTP_PASSWORD)
+
+    server.sendmail(SMTP_EMAIL, RECIPIENTS, message.as_string())
+
+    server.quit()
+
+    print("="*60)
+    print("Email notification sent successfully.")
+    print("="*60)
+    print(f"Recipients : {len(RECIPIENTS)}")
+
+    for recipient in RECIPIENTS:
+        print(recipient)
+except Exception as e:
+    print("="*60)
+    print("Failed to send email notification.")
+    print("="*60)
+    print(e)
+    raise
+
